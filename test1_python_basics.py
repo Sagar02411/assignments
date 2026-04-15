0fe4f17e5e94303b6b715249960fc7a033fd7a0c
anshika-1-2 <anshikapatel@vebuin.com>
2026-04-15 10:51:38 +0530

    updates

--- test1_python_basics.py
+++ test1_python_basics.py
@@ -211,12 +211,17 @@ class BankAccount:
     def __init__(self, deposit, balance):
         self.deposit = deposit
         self.__balance = balance # Private
-
-
+        
     def get_balance(self):
         return self.__balance
     
-    
+    def deposit(self):
+        amount = float(input("Enter amount: "))
+        self.__balance += amount
+        print("\nAmount Deposited:", amount)
+        
+
+
 # =========================
 # Q13: ABSTRACTION
 # =========================
