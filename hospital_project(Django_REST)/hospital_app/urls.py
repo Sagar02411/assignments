588959c848b9f3c1a6cf4c6cdd061d11b197dfe8
Your Name <devanshupadhyay@vebuin.com>
2026-05-19 12:39:27 +0530

    Django REST framework

---
+++ hospital_project(Django_REST)/hospital_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from . import views
+from rest_framework.routers import DefaultRouter
+from .views import PatientViewSet, DoctorViewSet, AppointmentViewSet
+
+router = DefaultRouter()
+router.register(r'patients', PatientViewSet)
+router.register(r'doctors', DoctorViewSet)
+router.register(r'appointments', AppointmentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
