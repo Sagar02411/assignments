73bf7a7ef85b8160fc286a2241692d7121be78a4
Your Name <devanshupadhyay@vebuin.com>
2026-05-19 12:44:10 +0530

    Final Commit to update changes

--- elibrary_project/elibrary_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from elibrary_app.models import Author,Category,EBooksModel
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Author)
-admin.site.register(EBooksModel)
--- elibrary_project/elibrary_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ElibraryAppConfig(AppConfig):
-    name = 'elibrary_app'
